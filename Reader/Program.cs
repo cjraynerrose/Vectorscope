d261ad470846d3e5dda496d9b4587f21988815a2
root <cjraynerrose@gmail.com>
2018-08-20 22:04:08 +0100

    Initial Commit
    Currently reads a WAV file and recognises valid WAV format.
    
    Next item is finding the left and right samples and automatically scaling the sample size when doing this.

---
+++ Reader/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+
+namespace Reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = ReadBytes(@"E:\dev_files\Yamaha-V50-Synbass-1-C2.wav");
+
+            var wav = Wav.CreateWav(data);
+
+            var ChunkId = wav.ChunkId;
+            var ChunkSize = wav.ChunkSize;
+            var Format = wav.Format;
+            var Subchunk1Id = wav.Subchunk1Id;
+            var Subchunk1Size = wav.Subchunk1Size;
+            var AudioFormat = wav.AudioFormat;
+            var NumChannels = wav.NumChannels;
+            var SampleRate = wav.SampleRate;
+            var ByteRate = wav.ByteRate;
+            var BlockAlign = wav.BlockAlign;
+            var BitsPerSample = wav.BitsPerSample;
+            var Subchunk2Id = wav.Subchunk2Id;
+            var Subchunk2Size = wav.Subchunk2Size;
+
+            string str_chunkId = GetBytesAsString(wav._ChunkId);
+            string str_chunkSize = GetBytesAsString(wav._ChunkSize);
+            string str_format = GetBytesAsString(wav._Format);
+            string str_sub1Id = GetBytesAsString(wav._Subchunk1Id);
+            string str_sub1Size = GetBytesAsString(wav._Subchunk1Size);
+            string str_audioForm = GetBytesAsString(wav._AudioFormat);
+            string str_numChan = GetBytesAsString(wav._NumChannels);
+            string str_sRate = GetBytesAsString(wav._SampleRate);
+            string str_bRate = GetBytesAsString(wav._ByteRate);
+            string str_align = GetBytesAsString(wav._BlockAlign);
+            string str_bps = GetBytesAsString(wav._BitsPerSample);
+            string str_sub2Id = GetBytesAsString(wav._Subchunk2Id);
+            string str_sub2Size = GetBytesAsString(wav._Subchunk2Size);
+            string str_data = GetBytesAsString(wav._RawData);
+
+            Console.WriteLine(
+                $"ChunkId       : {ChunkId}\t:: {str_chunkId}\n" +
+                $"ChunkSize     : {ChunkSize}\t:: {str_chunkSize}\n" +
+                $"Format        : {Format}\t:: {str_format}\n" +
+                $"Subchunk1Id   : {Subchunk1Id}\t:: {str_sub1Id}\n" +
+                $"Subchunk1Size : {Subchunk1Size}\t:: {str_sub1Size}\n" +
+                $"AudioFormat   : {AudioFormat}\t:: {str_audioForm}\n" +
+                $"NumChannels   : {NumChannels}\t:: {str_numChan}\n" +
+                $"SampleRate    : {SampleRate}\t:: {str_sRate}\n" +
+                $"ByteRate      : {ByteRate}\t:: {str_bRate}\n" +
+                $"BlockAlign    : {BlockAlign}\t:: {str_align}\n" +
+                $"BitsPerSample : {BitsPerSample}\t:: {str_bps}\n" +
+                $"Subchunk2Id   : {Subchunk2Id}\t:: {str_sub2Id}\n" +
+                $"Subchunk2Size : {Subchunk2Size}\t:: {str_sub2Size}\n\n" +
+                $"Raw Data      : {str_data.Substring(0, 254)}...");
+
+        }
+
+        public static byte[] ReadBytes(string filePath)
+        {
+            byte[] data;
+
+            if (!File.Exists(filePath))
+                throw new FileNotFoundException("File Not Found", filePath);
+
+            data = File.ReadAllBytes(filePath);
+
+            return data;
+        }
+
+        private static string GetBytesAsString(byte[] bytes)
+        {
+            return BitConverter.ToString(bytes);
+        }
+    }
+
+
+}
---
+++ Reader/Wav.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace Reader
+{
+    public class Wav
+    {
+        private byte[] _ChunkId;
+        private byte[] _ChunkSize;
+        private byte[] _Format;
+        private byte[] _Subchunk1Id;
+        private byte[] _Subchunk1Size;
+        private byte[] _AudioFormat;
+        private byte[] _NumChannels;
+        private byte[] _SampleRate;
+        private byte[] _ByteRate;
+        private byte[] _BlockAlign;
+        private byte[] _BitsPerSample;
+        private byte[] _Subchunk2Id;
+        private byte[] _Subchunk2Size;
+        private byte[] _Data;
+        private byte[] _RawData;
+
+        #region Getter Convertions
+
+        public string ChunkId { get { return System.Text.Encoding.ASCII.GetString(_ChunkId); } }
+        public int ChunkSize { get { return BitConverter.ToInt32(_ChunkSize, 0); } }
+        public string Format { get { return System.Text.Encoding.ASCII.GetString(_Format); } }
+        public string Subchunk1Id { get { return System.Text.Encoding.ASCII.GetString(_Subchunk1Id); } }
+        public int Subchunk1Size { get { return BitConverter.ToInt32(_Subchunk1Size, 0); } }
+        public int AudioFormat { get { return BitConverter.ToInt16(_AudioFormat, 0); } }
+        public int NumChannels { get { return BitConverter.ToInt16(_NumChannels, 0); } }
+        public int SampleRate { get { return BitConverter.ToInt32(_SampleRate, 0); } }
+        public int ByteRate { get { return BitConverter.ToInt32(_ByteRate, 0); } }
+        public int BlockAlign { get { return BitConverter.ToInt16(_BlockAlign, 0); } }
+        public int BitsPerSample { get { return BitConverter.ToInt16(_BitsPerSample, 0); } }
+        public string Subchunk2Id { get { return System.Text.Encoding.ASCII.GetString(_Subchunk2Id); } }
+        public int Subchunk2Size { get { return BitConverter.ToInt32(_Subchunk2Size, 0); } }
+        public byte[] Data { get { return _Data; } }
+
+        public byte[] LeftSamples { get; }
+        public byte[] RightSamples { get; }
+
+        #endregion
+
+        #region Creation Helpers
+
+        public static Wav CreateWav(byte[] rawData)
+        {
+            var wav = new Wav()
+            {
+                _ChunkId = SubArray(rawData, 0, 4),
+                _ChunkSize = SubArray(rawData, 4, 4),
+                _Format = SubArray(rawData, 8, 4),
+                _Subchunk1Id = SubArray(rawData, 12, 4),
+                _Subchunk1Size = SubArray(rawData, 16, 4),
+                _AudioFormat = SubArray(rawData, 20, 2),
+                _NumChannels = SubArray(rawData, 22, 2),
+                _SampleRate = SubArray(rawData, 24, 4),
+                _ByteRate = SubArray(rawData, 28, 4),
+                _BlockAlign = SubArray(rawData, 32, 2),
+                _BitsPerSample = SubArray(rawData, 34, 2),
+                _Subchunk2Id = SubArray(rawData, 36, 4),
+                _Subchunk2Size = SubArray(rawData, 40, 4),
+                _Data = SubArray(rawData, 44, rawData.Length - 44),
+                _RawData = rawData,
+            };
+
+            if (wav.NumChannels > 1)
+                wav = GetDualChannel();
+
+            if (ValidateWav(wav))
+                return wav;
+            else
+                throw new FormatException("File not in correct WAV format.");
+        }
+
+        private static T[] SubArray<T>(T[] data, int index, int length)
+        {
+            T[] result = new T[length];
+            Array.Copy(data, index, result, 0, length);
+            return result;
+        }
+
+        private static bool ValidateWav(Wav wav)
+        {
+            bool result = true;
+
+            if (wav.ChunkId != "RIFF") result = false;
+            if (wav.Format != "WAVE") result = false;
+            if (wav.Subchunk1Id != "fmt ") result = false;
+            if (wav.Subchunk2Id != "data") result = false;
+            if (wav.ChunkId != "RIFF") result = false;
+
+            return result;
+        }
+
+        private static Wav GetDualChannel(Wav wav)
+        {
+            var data = wav.Data;
+            var dataLength = data.Length;
+
+            var leftSample = true;
+            var left = new byte[dataLength / 2];
+            var right = new byte[dataLength / 2];
+
+            for(int i=0;i<dataLength;i+=4)
+            {
+                
+            }
+
+            return wav;
+        }
+
+        #endregion
+    }
+}
